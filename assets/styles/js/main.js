1b9e5d7d74432219f3017f2ab85a9e0f505e56bf
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-10-20 00:56:30 +0530

    Add Project section Js

--- assets/styles/js/main.js
+++ assets/styles/js/main.js
@@ -167,14 +167,14 @@
         itemSelector: '.portfolio-item'
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select('#portfolio-filters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-filters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
+          el.classList.remove('filters-active');
         });
-        this.classList.add('filter-active');
+        this.classList.add('filters-active');
 
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
